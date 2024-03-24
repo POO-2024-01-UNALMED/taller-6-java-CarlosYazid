e7367ee6ad51f815dd02135827aa5e11a0c66fe0
Carlos Yazid <140143753+CarlosYazid@users.noreply.github.com>
2024-03-24 01:12:10 +0000

    OK

--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -18,7 +18,7 @@ public class Camioneta extends Vehiculo {
     public Camioneta(String placa, int puertas, String nombre, int precio, float peso,Fabricante fabricante,boolean volco) {
         super(placa,puertas, 90,nombre,precio,peso,"4X4",fabricante);
         this.volco = volco;
-        Camioneta.cantidadCamioneta++;
+        Camioneta.cantidadCamionetas++;
     }
 
     public Camioneta() {this(null,0,null,0,0,null,false);}
